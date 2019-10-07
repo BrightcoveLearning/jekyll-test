b7c0fccd5e79cf19ba5396ce311f106ddd763fd1
Robert Crooks <rcrooks@brightcove.com>
2019-10-07 14:57:08 -0400

    wip

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,14 @@
 var BCLS_site = ( function (window, document) {
   var sidenav_headers = document.querySelectorAll('.sidenav h5'),
-    sidenav_submenus = document.querySelectorAll('.sidenav ul');
+    sidenav_submenus = document.querySelectorAll('.sidenav ul'),
+    i,
+    iMax;
+
+    function show_submenu(idx) {
+      // first hide all
+      iMax = sidenav_submenus.length;
+      for (i = 0; i < iMax; i++) {
+        sidenav_submenus[i].setAttribute('style', 'display:none;')
+      }
+    }
 })(window, document);
